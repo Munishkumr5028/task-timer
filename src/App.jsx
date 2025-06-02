1015b7c95a5dd741c30e9a71c63e56616341e354
Munishkumr5028 <160215415+Munishkumr5028@users.noreply.github.com>
2025-06-02 10:53:35 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useLayoutEffect } from "react";
 import Counter from "./components/Counter";
 import "./App.css";
 
 export default function App() {
   const inputRef = useRef(null);
   const inputValueRef = useRef("");
-
   const [focusCount, setFocusCount] = useState(0);
 
   useEffect(() => {
-    console.log("Component mounted - useRef used for input focus & value");
+    console.log("Component mounted ");
+  }, []);
+
+  useLayoutEffect(() => {
+    if (inputRef.current) {
+      const height = inputRef.current.getBoundingClientRect().height;
+      console.log("useLayoutEffect: Input height =", height);
+    }
   }, []);
 
   const handleFocus = () => {
@@ -59,70 +65,3 @@ export default function App() {
     </div>
   );
 }
-
-// import { useRef, useState, useEffect } from "react";
-// import Counter from "./components/Counter";
-// import "./App.css";
-
-// export default function App() {
-//   const inputRef = useRef("");
-
-//   const [inputValue, setInputValue] = useState("");
-
-//   const [focusCount, setFocusCount] = useState(0);
-
-//   useEffect(() => {
-//     console.log("Component mounted");
-//   }, []);
-
-//   useEffect(() => {
-//     console.log("Re-render due to inputValue state change:", inputValue);
-//   }, [inputValue]);
-
-//   const handleFocus = () => {
-//     setFocusCount((prev) => prev + 1);
-//   };
-
-//   const handleChange = (e) => {
-//     setInputValue(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Submitted value (useState):", inputValue);
-//   };
-
-//   const focusInput = () => {
-//     document.querySelector(".app-input").focus();
-//     console.log("focusInput called: input focused");
-//   };
-
-//   return (
-//     <div className="app-container">
-//       <h1 className="app-title">useReducer + useRef</h1>
-
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           className="app-input"
-//           placeholder="Click button or focus me"
-//           onFocus={handleFocus}
-//           onChange={handleChange}
-//           value={inputValue}
-//         />
-//         <button className="app-button" type="submit">
-//           Submit
-//         </button>
-//       </form>
-
-//       <button className="app-button" onClick={focusInput}>
-//         Focus Input
-//       </button>
-
-//       <p>Input focus count (renders): {focusCount}</p>
-
-//       <p>Live input value: {inputValue}</p>
-
-//       <Counter />
-//     </div>
-//   );
-// }
